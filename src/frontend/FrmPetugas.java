7bc21a0970cb555300a34700ac8d267e59242f59
Abyan <harlem.minion09@gmail.com>
2019-10-24 11:44:02 +0700

    menambahkan cetak di Petugas

--- src/frontend/FrmPetugas.java
+++ src/frontend/FrmPetugas.java
@@ -6,6 +6,7 @@
 package frontend;
 import backend.Petugas;
 import java.awt.event.KeyEvent;
+import java.awt.print.PrinterException;
 import java.util.ArrayList;
 import javax.swing.ButtonModel;
 import javax.swing.JOptionPane;
@@ -164,6 +165,7 @@ public class FrmPetugas extends javax.swing.JFrame {
         txtPassword = new javax.swing.JTextField();
         jScrollPane3 = new javax.swing.JScrollPane();
         txtAlamat = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
         jMenuBar2 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuHome = new javax.swing.JMenuItem();
@@ -350,7 +352,7 @@ public class FrmPetugas extends javax.swing.JFrame {
         });
 
         jLabel9.setFont(new java.awt.Font("Algerian", 1, 24)); // NOI18N
-        jLabel9.setText("Menu Kategori Buku");
+        jLabel9.setText("Menu Daftar Petugas");
 
         jLabel7.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel7.setText("Username");
@@ -375,6 +377,13 @@ public class FrmPetugas extends javax.swing.JFrame {
         txtAlamat.setColumns(20);
         txtAlamat.setRows(5);
         jScrollPane3.setViewportView(txtAlamat);
+
+        jButton1.setText("Cetak");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jMenu1.setText("Menu");
 
@@ -505,7 +514,8 @@ public class FrmPetugas extends javax.swing.JFrame {
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                         .addComponent(txtCari, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(btnCari))))
+                                        .addComponent(btnCari))
+                                    .addComponent(jButton1)))
                             .addComponent(jLabel9))
                         .addGap(0, 234, Short.MAX_VALUE)))
                 .addContainerGap())
@@ -531,6 +541,8 @@ public class FrmPetugas extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 299, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1)
                         .addContainerGap())
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -699,6 +711,20 @@ public class FrmPetugas extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_tblPetugasKeyPressed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        try{
+            boolean complate = tblPetugas.print();
+            if(complate){
+                JOptionPane.showMessageDialog(null, "Sukses: ");
+            }else{
+                JOptionPane.showMessageDialog(null, "Gagal");
+            }
+        }catch(PrinterException e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -759,6 +785,7 @@ public class FrmPetugas extends javax.swing.JFrame {
     private javax.swing.JButton btnTambahBaru;
     private javax.swing.ButtonGroup groupGender;
     private javax.swing.ButtonGroup groupStudi;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
